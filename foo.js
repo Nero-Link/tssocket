579a1a7e850706794a50b34908ba945bc680e0e8
Brad Inch <nerolink1107@gmail.com>
2022-09-20 13:42:17 +1000

    initial commit

---
+++ foo.js
@@ -0,0 +1,20 @@
+var Grault = /** @class */ (function () {
+    function Grault(quux, waldo) {
+        this.garply = quux.quuz + " " + quux.corge + " " + waldo;
+    }
+    Grault.prototype.getGarply = function () {
+        return this.garply;
+    };
+    return Grault;
+}());
+function foo(bar) {
+    return "Hello, " + bar.quuz + " " + bar.corge;
+}
+var baz = { quuz: "ABC", corge: 123 };
+var fred = new Grault(baz, [1, 2, 3]);
+console.log(foo(baz));
+console.log(fred.getGarply());
+try {
+    document.body.innerHTML = fred.getGarply();
+}
+catch (e) { }
---
+++ foo.ts
@@ -0,0 +1,32 @@
+class Grault {
+  private garply: string;
+
+  constructor(quux: Quux, waldo: number[]) {
+    this.garply = quux.quuz + " " + quux.corge + " " + waldo;
+  }
+
+  public getGarply() {
+    return this.garply;
+  }
+}
+
+interface Quux {
+  quuz: string;
+  corge: number;
+}
+
+function foo(bar: Quux) {
+  return "Hello, " + bar.quuz + " " + bar.corge;
+}
+
+let baz: Quux = { quuz: "ABC", corge: 123 };
+
+let fred: Grault = new Grault(baz, [1, 2, 3]);
+
+console.log(foo(baz));
+
+console.log(fred.getGarply());
+
+try {
+  document.body.innerHTML = fred.getGarply();
+} catch (e) {}
